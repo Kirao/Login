a75951f2807c7d56b68733ec234329032c9a90ed
Guilherme Henrique Bodo <guihenrique1231@hotmail.com>
2021-02-20 02:42:18 -0300

    Add files via upload
    Começo de um programa de Login :3

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bom dia,Digite seu Usuario para Login.. ");
+            var nome = Console.ReadLine(); //Digitar o nome do usuario
+            Console.WriteLine($"\nOla, {nome}!Digite sua senha.");
+            var Senha = Console.ReadLine(); //setar uma senha para o usuario
+            Console.WriteLine($"\nOla, {nome}!Confirme sua senha.");
+            var pass = Console.ReadLine();//Confirmar a senha do usuario
+            
+            if(Senha == pass)//execução caso a senha seja correta
+            {
+              Console.WriteLine("Bem vindo, Guilherme, Tenha um otimo serviço!");                    
+             }
+            else //execução caso a senha seja incorreta
+            {
+             Console.WriteLine("Senha incorreta..");             
+            }
+
+        }
+        
+        }
+    }
